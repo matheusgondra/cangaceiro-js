65049bc0971751fc920eb2a618b2295c89b01ffd
matheusgondra <matheusgondra951@gmail.com>
2022-10-16 12:16:13 -0300

    Finalizando capitulo 8

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -4,7 +4,7 @@ class NegociacaoController {
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
-		this._negociacoes = new Negociacoes();
+		this._negociacoes = new Negociacoes(model =>	this._negociacoesView.update(model) );
 		this._negociacoesView = new NegociacoesView("#negociacoes");
 		this._negociacoesView.update(this._negociacoes);
 		this._mensagem = new Mensagem();
@@ -16,7 +16,6 @@ class NegociacaoController {
 		event.preventDefault();
 		this._negociacoes.adiciona(this._criaNegociacao());
 		this._mensagem.texto = "Negociação adicionada com sucesso";
-		this._negociacoesView.update(this._negociacoes);
 		this._mensagemView.update(this._mensagem);
 		this._limpaFormulario();
 	}
@@ -34,5 +33,11 @@ class NegociacaoController {
 			parseInt(this._inputQuantidade.value),
 			parseFloat(this._inputValor.value)
 		);
+	}
+
+	apaga() {
+		this._negociacoes.esvazia();
+		this._mensagem.texto = "Negociações apagadas com sucesso";
+		this._mensagemView.update(this._mensagem);
 	}
 }
--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,10 +1,13 @@
 class Negociacoes {
-	constructor() {
+	constructor(armadilha) {
 		this._negociacoes = [];
+		this._armadilha = armadilha;
+		Object.freeze(this);
 	}
 
 	adiciona(negociacao) {
 		this._negociacoes.push(negociacao);
+		this._armadilha(this);
 	}
 
 	paraArray() {
@@ -13,5 +16,10 @@ class Negociacoes {
 
 	get volumeTotal() {
 		return this._negociacoes.reduce((total, negociacao) => total + negociacao.volume, 0);
+	}
+
+	esvazia() {
+		this._negociacoes.length = 0;
+		this._armadilha(this);
 	}
 }
