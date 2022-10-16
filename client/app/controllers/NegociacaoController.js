e20d55ca37e3ed265e97ee7d6900fc9003c78e73
matheusgondra <matheusgondra951@gmail.com>
2022-10-15 22:14:54 -0300

    Finalizando capitulo 6

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,11 +5,14 @@ class NegociacaoController {
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
 		this._negociacoes = new Negociacoes();
+		this._negociacoesView = new NegociacoesView("#negociacoes");
+		this._negociacoesView.update(this._negociacoes);
 	}
 
 	adiciona(event) {
 		event.preventDefault();
 		this._negociacoes.adiciona(this._criaNegociacao());
+		this._negociacoesView.update(this._negociacoes);
 		this._limpaFormulario();
 	}
 
