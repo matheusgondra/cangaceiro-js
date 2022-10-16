3ccebb1fee5551309ef60712c6ff620676cac625
matheusgondra <matheusgondra951@gmail.com>
2022-10-15 21:51:50 -0300

    Finalizando capitulo 5

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -4,24 +4,27 @@ class NegociacaoController {
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
+		this._negociacoes = new Negociacoes();
 	}
 
 	adiciona(event) {
 		event.preventDefault();
+		this._negociacoes.adiciona(this._criaNegociacao());
+		this._limpaFormulario();
+	}
 
-		let data = new Date(
-			...this._inputData.value
-				.split("-")
-				.map((item, indice) => item - indice % 2)
-		);
+	_limpaFormulario() {
+		this._inputData.value = "";
+		this._inputQuantidade.value = 1;
+		this._inputValor.value = 0.0;
+		this._inputData.focus();
+	}
 
-		let negociacao = new Negociacao(
-			data,
+	_criaNegociacao() {
+		return new Negociacao(
+			DateConverter.paraData(this._inputData.value),
 			parseInt(this._inputQuantidade.value),
 			parseFloat(this._inputValor.value)
 		);
-		
-		let diaMesAno = DateConverter.paraTexto(negociacao.data);
-		console.log(diaMesAno)
 	}
 }
