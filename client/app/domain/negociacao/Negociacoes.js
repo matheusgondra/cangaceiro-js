6e93989294ea13e6d4cc3c1fcdd65df0f95a4ce3
matheusgondra <matheusgondra951@gmail.com>
2022-10-16 13:11:15 -0300

    Finalizando capitulo 9

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,13 +1,11 @@
 class Negociacoes {
 	constructor(armadilha) {
 		this._negociacoes = [];
-		this._armadilha = armadilha;
 		Object.freeze(this);
 	}
 
 	adiciona(negociacao) {
 		this._negociacoes.push(negociacao);
-		this._armadilha(this);
 	}
 
 	paraArray() {
@@ -20,6 +18,5 @@ class Negociacoes {
 
 	esvazia() {
 		this._negociacoes.length = 0;
-		this._armadilha(this);
 	}
 }
