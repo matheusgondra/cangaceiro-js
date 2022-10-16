123534f59b41d173b30a23603b7a6098688adf73
matheusgondra <matheusgondra951@gmail.com>
2022-10-15 22:37:25 -0300

    Finalizando capitulo 7

--- client/app/app.js
+++ client/app/app.js
@@ -1,3 +1,3 @@
-let controller = new NegociacaoController();
+const controller = new NegociacaoController();
 
 document.querySelector(".form").addEventListener("submit", controller.adiciona.bind(controller));
--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,18 +1,23 @@
 class NegociacaoController {
 	constructor() {
-		let $ = document.querySelector.bind(document);
+		const $ = document.querySelector.bind(document);
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
 		this._negociacoes = new Negociacoes();
 		this._negociacoesView = new NegociacoesView("#negociacoes");
 		this._negociacoesView.update(this._negociacoes);
+		this._mensagem = new Mensagem();
+		this._mensagemView = new MensagemView("#mensagemView");
+		this._mensagemView.update(this._mensagem);
 	}
 
 	adiciona(event) {
 		event.preventDefault();
 		this._negociacoes.adiciona(this._criaNegociacao());
+		this._mensagem.texto = "Negociação adicionada com sucesso";
 		this._negociacoesView.update(this._negociacoes);
+		this._mensagemView.update(this._mensagem);
 		this._limpaFormulario();
 	}
 
