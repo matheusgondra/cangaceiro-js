b3fd83d650cb66f4e940e22807553db5eebdb536
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-10 07:14:08 -0300

    Finalizando capituilo 19

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -2,10 +2,8 @@ import { NegociacaoController } from "./controllers/NegociacaoController.js";
 import { Negociacao } from "./domain/index.js";
 
 const controller = new NegociacaoController();
-const $ = document.querySelector.bind(document);
 
 const negociacao = new Negociacao(new Date(), 1, 200);
-
 const headers = new Headers();
 headers.set("Content-Type", "application/json");
 const body = JSON.stringify(negociacao);
@@ -19,7 +17,3 @@ const config = {
 
 fetch("/negociacoes", config)
 	.then(() => console.log("Dado enviado com sucesso"));
-
-$(".form").addEventListener("submit", controller.adiciona.bind(controller));
-$("#botao-apaga").addEventListener("click", controller.apaga.bind(controller));
-$("#botao-importa").addEventListener("click", controller.importaNegociacoes.bind(controller));
